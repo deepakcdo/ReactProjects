98915f2e44b2dd24d896b3d3ab3840cc66a3fbff
Deepak <deepakcdo@gmail.com>
2019-07-29 21:44:39 +0100

    Created new burger-buidler app - Auxx and Layout js files

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Layout from './components/Layout/Layout'
+
 function App() {
   return (
     <div >
+      <Layout>
     <h1>Hello app</h1>
+      </Layout>
     </div>
   );
 }
